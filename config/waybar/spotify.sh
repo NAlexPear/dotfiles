cee97195bfd79084691bb8d46963d2c137753d4c
Alex Pearson <alex@alexpear.com>
2022-02-17 11:38:43 -0500

    ✨ Switch from rofi to wofi

--- config/waybar/spotify.sh
+++ config/waybar/spotify.sh
@@ -4,35 +4,38 @@ elapsed="0"
 cache=""
 
 while [[ $(ps -u $USER | grep spotify) ]]; do
-  title=`exec playerctl metadata xesam:title`
-  artist=`exec playerctl metadata xesam:artist`
-  album=`exec playerctl metadata xesam:album`
   length=`exec playerctl metadata mpris:length`
 
-  secs=$(($length/1000000))
-  timestamp=`printf ""%d:%02d"" $(($secs%3600/60)) $(($secs%60))`
+  if [[ $length ]]; then
+    title=`exec playerctl metadata xesam:title`
+    artist=`exec playerctl metadata xesam:artist`
+    album=`exec playerctl metadata xesam:album`
+    secs=$(($length/1000000))
+    timestamp=`printf ""%d:%02d"" $(($secs%3600/60)) $(($secs%60))`
 
+    function update_cache {
+      if [ "$cache" != "$artist $title $album $length" ]; then
+        cache="$artist $title $album $length"
+        elapsed="0"
+      fi
+    }
 
-  function update_cache {
-    if [ "$cache" != "$artist $title $album $length" ]; then
-      cache="$artist $title $album $length"
-      elapsed="0"
+    function get_info {
+      position=`printf ""%d:%02d"" $(($elapsed%3600/60)) $(($elapsed%60))`
+
+      echo "$title <span style=\"italic\">by</span> $artist $album  $position $timestamp"
+    }
+
+    if [ "$(playerctl status)" = "Playing" ]; then
+      elapsed=$(($elapsed + 1))
+      update_cache
+
+      echo "⏸ $(get_info)"
+    else
+      echo "▶ $(get_info)"
     fi
-  }
-
-  function get_info {
-    position=`printf ""%d:%02d"" $(($elapsed%3600/60)) $(($elapsed%60))`
-
-    echo "$title <span style=\"italic\">by</span> $artist $album  $position $timestamp"
-  }
-
-  if [ "$(playerctl status)" = "Playing" ]; then
-    elapsed=$(($elapsed + 1))
-    update_cache
-
-    echo "⏸ $(get_info)"
   else
-    echo "▶ $(get_info)"
+    echo ""
   fi
 
   sleep 1
