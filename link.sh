d719004ed6a1b52eb2f1a8103d0c851e29779418
Alex Pearson <alex@alexpear.com>
2024-02-11 13:54:35 -0500

    ✨ Add .desktop application entries for better app launching

--- link.sh
+++ link.sh
@@ -4,11 +4,16 @@ set -o errexit
 set -o pipefail
 
 CONFIG="$HOME/.config"
+APPS="$HOME/.local/share/applications"
+APP_SOURCE="$(dirname $0)/local/share/applications"
 CONFIG_SOURCE="$(dirname $0)/config"
 HOME_SOURCE="$(dirname $0)/home"
 
 # create the .config directory if it doesn't exist
 mkdir -p "$CONFIG"
+
+# create the applications directory if it doesn't exist
+mkdir -p "$APPS"
 
 # symlink top-level .config subdirectories and files
 while read -r ITEM; do
@@ -19,3 +24,8 @@ done < <(ls $CONFIG_SOURCE)
 while read -r ITEM; do
   ln -nsf "$(readlink -m $HOME_SOURCE/$ITEM)" "$HOME/$ITEM"
 done < <(ls $HOME_SOURCE)
+
+# symlink .desktop files into the local applications directory
+while read -r ITEM; do
+  ln -nsf "$(readlink -m $APP_SOURCE/$ITEM)" "$APPS/$ITEM"
+done < <(ls $APP_SOURCE | rg -e desktop)
